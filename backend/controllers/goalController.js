4e6876428226aabf96d296d77e824d286709f031
sagar-pardhi <sgrpardhi007@gmail.com>
2022-02-22 19:09:59 +0530

    added controllers

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc    GET goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+// @desc    SET goals
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goals" });
+};
+
+// @desc    Update goals
+// @route   PUT /api/goals
+// @access  Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goals ${req.params.id}` });
+};
+
+// @desc    Delete goals
+// @route   DELETE /api/goals
+// @access  Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goals ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
