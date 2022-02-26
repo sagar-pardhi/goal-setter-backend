076f71a7fb2bb0c3aa376d9979bfc888f748293f
sagar-pardhi <sgrpardhi007@gmail.com>
2022-02-26 20:11:37 +0530

    added jwt token in register and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -60,6 +61,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -73,6 +75,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "user data" });
 });
+
+// generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
