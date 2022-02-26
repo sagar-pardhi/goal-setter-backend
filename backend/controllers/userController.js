97cf93c1d416d6fa9bf1cbaa60d1d0ffe5fee30a
sagar-pardhi <sgrpardhi007@gmail.com>
2022-02-26 20:06:43 +0530

    added login user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
     });
   } else {
-    res.send(400);
+    res.status(400);
     throw new Error("Invalid user data");
   }
 });
@@ -49,7 +49,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+
+  // check user email
+  const user = await User.findOne({ email });
+
+  // check password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // @desc    Get user data
